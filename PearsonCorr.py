7af6dde8a398a52dd65b0ecdb48178701159cedc
Mustafa Berkay Sökmen <131465305+MustafaBerkaySokmen@users.noreply.github.com>
2025-01-04 16:33:14 +0300

    Add files via upload

---
+++ PearsonCorr.py
@@ -0,0 +1,53 @@
+def calc_mean(X):
+    '''
+    IMPLEMENT THIS FUNCTION.
+    '''
+    return -999 # replace this line
+ 
+        
+
+def calc_stdev(X):
+    '''
+    IMPLEMENT THIS FUNCTION.
+    '''
+    return -999 # replace this line
+
+
+
+def calc_cov(X, Y): 
+    '''
+    IMPLEMENT THIS FUNCTION.
+    '''
+    return -999 # replace this line
+
+
+
+def calc_pearson(X, Y):
+    '''
+    IMPLEMENT THIS FUNCTION.
+    '''
+    return -999 # replace this line
+ 
+
+def main():
+ 	'''
+    You can test your implementations using the function calls here.
+    These function calls are here only to help you test your function.
+    '''
+    
+    X = [1.5, 3.2, 0.8]
+    Y = [1.9, 4.5, 6.0]
+    print('mean for X:',calc_mean(X))
+    print('mean for Y:',calc_mean(Y))
+    print('std for X:',calc_stdev(X))
+    print('std for Y:',calc_stdev(Y))
+    print('covariance:',calc_cov(X,Y))
+    print('pearson:',calc_pearson(X,Y))
+    
+   
+################################################################ 
+'''
+DO NOT EDIT BELOW THIS LINE
+'''
+main()
+
